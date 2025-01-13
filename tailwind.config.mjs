fbf59fd747790f3935c5f075bcebb911f7987fa1
Jack <39860321+jackstirzaker@users.noreply.github.com>
2025-01-13 15:03:47 +0000

    Remove favicon, change redirect and begin to add branded colours

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+    colors: {
+      // Configure your color palette here
+    }
 	},
 	plugins: [],
 }
