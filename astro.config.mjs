4587f382d487a53696ce8c9e79357c33e4a15f10
Jack <39860321+jackstirzaker@users.noreply.github.com>
2025-01-24 17:57:04 +0000

    Attempt vCard generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind({
+    applyBaseStyles: false,
+    nesting: true,
+  })],
 
   site: 'https://card.gotos.me',
 });
